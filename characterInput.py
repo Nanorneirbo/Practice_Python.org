c69e9c0dabec716a9c9e77a45a9e505b280b4827
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-05 21:03:27 +0000

    first py

---
+++ characterInput.py
@@ -0,0 +1,9 @@
+# https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
+print('What is your name?')
+userName = input()
+print('What age are you?')
+age = input()
+centYear = 100-age
+turn100 = 2019+centYear
+print(userName + ' will turn 100 in the year ' + str(turn100))
