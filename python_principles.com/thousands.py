386d37058f91ddf8d4768d008980b93b7d675cba
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-14 17:16:04 +0000

    first comma position loop - working

--- python_principles.com/thousands.py
+++ python_principles.com/thousands.py
@@ -24,7 +24,7 @@ def format_number(number):
         else:
             check_number = False
 
-    # do the string - modding off the back 3
+    # do the string - first 1, 2, 3 digits
     if check_number == True:
         first_comma_position = no_0f_digits % 3
 
@@ -36,16 +36,39 @@ def format_number(number):
             else:
                 new_string = new_string + number[0] + number[1] + ","
 
+        else:
+            new_string = number
 
+        # rest of the number
 
-        print(new_string)
+        end_number_string = ""
+
+        #todo: change first comma position to 3 - will allow to build same loop for all three scenarios
+
+        if first_comma_position == 0:
+            no_of_commas = int(no_0f_digits/3)
+            # skip first comma
+            for i in range(1, no_of_commas):
+                end_number_string = end_number_string + number[0+(i*3)] + number[1+(i*3)] + number[2+(i*3)] + ","
+        elif first_comma_position == 1:
+            no_of_commas = int((no_0f_digits-1)/3)
+            for i in range(no_of_commas):
+                end_number_string = end_number_string + number[1+(i*3)] + number[2+(i*3)] + number[3+(i*3)] + ","
+        # first comma pos 2
+        else:
+            no_of_commas = int((no_0f_digits - 2) / 3)
+            for i in range(no_of_commas):
+                end_number_string = end_number_string + number[2 + (i * 3)] + number[3 + (i * 3)] + number[
+                    4 + (i * 3)] + ","
 
 
     else:
         print("please enter a real number")
 
+    new_string = new_string + end_number_string
+
     return new_string
 
 
 
-format_number(input(" Enter a number please"))
+print(format_number(input(" Enter a number please")))
