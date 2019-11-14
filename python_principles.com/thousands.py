7e31c32456dbd64b8d354a3e48695d80c850d6f3
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-14 16:56:55 +0000

    do the first two digits

--- python_principles.com/thousands.py
+++ python_principles.com/thousands.py
@@ -5,30 +5,47 @@ Your function should convert the number to a string and add commas as a thousand
 For example, calling format_number(1000000) should return "1,000,000".'''
 
 
-number_input = int(input("enter a number"))
+
+
+def format_number(number):
+    number = str(number)
+    no_0f_digits = len(number)
+
+    # set up a string to check that all of the numbers are actually numbers
+    actual_numbers = "0123456789"
+
+    check_number = False
+
+    new_string = ""
+
+    for i in range(no_0f_digits):
+        if number[i] in actual_numbers:
+            check_number = True
+        else:
+            check_number = False
+
+    # do the string - modding off the back 3
+    if check_number == True:
+        first_comma_position = no_0f_digits % 3
+
+        if no_0f_digits > 3:
+            if first_comma_position == 0:
+                new_string = new_string + number[0] + number[1] + number[2] + ","
+            elif first_comma_position == 1:
+                new_string = new_string + number[0] + ","
+            else:
+                new_string = new_string + number[0] + number[1] + ","
 
 
 
-string_input = str(number_input)
+        print(new_string)
 
-no_0f_digits = len(string_input)
 
-# set up a string to check that all of the numbers are actually numbers
-actual_numbers = "0123456789"
-
-check_number = False
-
-for i in range(no_0f_digits):
-    if string_input[i] in actual_numbers:
-        check_number = True
     else:
-        check_number = False
+        print("please enter a real number")
 
-# do the sring
-if check_number == True:
+    return new_string
 
-else:
-    print("please enter a real number")
 
-print(no_0f_digits)
 
+format_number(input(" Enter a number please"))
